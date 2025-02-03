83073db40ef1609a4a1c12748d8047a502ff837e
Joseph Pal <josephpal27@gmail.com>
2025-02-03 15:31:59 +0530

    update index.html title and favicon; add weather functionality in App component; enhance styling with new CSS classes

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,65 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  let [city, setCity] = useState('')
+  let [weatherData, setWeatherData] = useState();
+
+  let checkWeather = (e) => {
+    e.preventDefault()
+
+    fetch(`http://api.weatherapi.com/v1/current.json?key=1da33a8c94004a8c96970835250302&q=${city}&aqi=no`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        if (data.error.code == 1006) {
+          alert('City not found')
+        } else {
+          setWeatherData(data)
+        }
+      })
+
+    setCity('');
+  }
+
+
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className="container">
+        <h1>Weather App</h1>
+        <div className="search-box">
+          <form action="" onSubmit={checkWeather}>
+            <input type="text" className="search" placeholder="Search Location..." value={city} onChange={(e) => setCity(e.target.value)} />
+            <button className="search-button">Search</button>
+          </form>
+        </div>
+
+        {weatherData != undefined
+
+          ?
+
+          <div className="app-body">
+            <h2>Kolkata</h2>
+            <img src="//cdn.weatherapi.com/weather/64x64/day/122.png" alt="icon" />
+            <h3>25°C</h3>
+            <h4>Last Updated On - 2025-02-03 12:30</h4>
+          </div>
+
+          :
+          
+          <div className='no-data'>
+            <p>No Data Found</p>
+          </div>
+        }
+
+        <div className="loader">
+          <img src="https://media.tenor.com/khzZ7-YSJW4AAAAM/cargando.gif" alt="loader" />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
